3b74b2f010fca81b188a2a574d4793300cae612f
ayomide.abiola <ayomide.abiola@cecureintel.com>
2025-11-21 19:12:26 +0100

    Fixing Issue

--- backend/infrastructure/modules/state/main.tf
+++ backend/infrastructure/modules/state/main.tf
@@ -2,6 +2,10 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.state_bucket_name
 
+  lifecycle {
+    prevent_destroy = true
+  }
+
   tags = {
     Name        = var.state_bucket_name
     Environment = var.environment
@@ -60,6 +64,10 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
   name         = var.state_lock_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 
   attribute {
     name = "LockID"
